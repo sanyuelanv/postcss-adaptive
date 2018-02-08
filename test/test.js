43621e664b8e6313054b5899a244b4385ee5acfc
宋航 <songhang@LiMeiyundeMBP.lan>
2018-02-09 01:02:25 +0800

    fix: warn

--- test/test.js
+++ test/test.js
@@ -14,32 +14,31 @@ describe('config', function () {
   it('normal', function () {
     var fixture = readFile('test/css.css')
     var output = postcss().use(adaptive()).process(fixture).css
-    console.log(output)
-    // expect(output).is.a.string
+    expect(output).is.a.string
     // expect(output).eql(expected)
   })
-  // // rpx 转换
-  // it('rem unit', function () {
-  //   var fixture = '.a { height: 64rpx; }'
-  //   var expected = '.a {\n  height: 1rem;\n}'
-  //   var output = postcss().use(adaptive({ remUnit: 64 })).process(fixture).css
-  //   expect(output).is.a.string
-  //   expect(output).eql(expected)
-  // })
-  // // px 不转换
-  // it('base dpr', function () {
-  //   var fixture = '.a { height: 75px; }'
-  //   var expected = '.a {\n  height: 75px;\n}'
-  //   var output = postcss().use(adaptive({ baseDpr: 3 })).process(fixture).css
-  //   expect(output).is.a.string
-  //   expect(output).eql(expected)
-  // })
-  // // rpx 转换 精确度
-  // it('rem precision', function () {
-  //   var fixture = '.a { height: 65rpx; }'
-  //   var expected = '.a {\n  height: 0.86666667rem;\n}'
-  //   var output = postcss().use(adaptive({ remPrecision: 8 })).process(fixture).css
-  //   expect(output).is.a.string
-  //   expect(output).eql(expected)
-  // })
+  // rpx 转换
+  it('rem unit', function () {
+    var fixture = '.a { height: 64rpx; }'
+    var expected = '.a {\n  height: 1rem;\n}'
+    var output = postcss().use(adaptive({ remUnit: 64 })).process(fixture).css
+    expect(output).is.a.string
+    expect(output).eql(expected)
+  })
+  // px 不转换
+  it('base dpr', function () {
+    var fixture = '.a { height: 75px; }'
+    var expected = '.a {\n  height: 75px;\n}'
+    var output = postcss().use(adaptive({ baseDpr: 3 })).process(fixture).css
+    expect(output).is.a.string
+    expect(output).eql(expected)
+  })
+  // rpx 转换 精确度
+  it('rem precision', function () {
+    var fixture = '.a { height: 65rpx; }'
+    var expected = '.a {\n  height: 0.86666667rem;\n}'
+    var output = postcss().use(adaptive({ remPrecision: 8 })).process(fixture).css
+    expect(output).is.a.string
+    expect(output).eql(expected)
+  })
 })
