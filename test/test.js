2adb539ec90845e62ab792f4cd08521c69110133
宋航 <songhang@LiMeiyundeMBP.lan>
2018-02-09 00:12:49 +0800

    fix: md

--- test/test.js
+++ test/test.js
@@ -4,56 +4,29 @@ var postcss = require('postcss')
 var expect = chai.expect
 var adaptive = require('../')
 
-function readFile (filepath) {
-  if (fs.existsSync(filepath)) {
-    return fs.readFileSync(filepath, { encoding: 'utf-8' }) || ''
-  }
-  return ''
-}
-
-// describe('integration', function () {
-
-//   it('normal', function () {
-//     var fixture = readFile('test/fixture.css')
-//     var expected = readFile('test/expected.css')
-//     var output = postcss().use(adaptive()).process(fixture).css
-//     expect(output).is.a.string
-//     expect(output).eql(expected)
-//   })
-
-//   it('auto rem', function () {
-//     var fixture = readFile('test/fixture-autorem.css')
-//     var expected = readFile('test/expected.css')
-//     var output = postcss().use(adaptive({ autoRem: true })).process(fixture).css
-//     expect(output).is.a.string
-//     expect(output).eql(expected)
-//   })
-// })
-
 describe('config', function () {
-
+  // rpx 转换
   it('rem unit', function () {
-    var fixture = '.a { height: 64rpx; width: 64px; }'
-    var expected = '.a {\n  height: 1rem;\n }'
+    var fixture = '.a { height: 64rpx; }'
+    var expected = '.a {\n  height: 1rem;\n}'
     var output = postcss().use(adaptive({ remUnit: 64 })).process(fixture).css
-    console.log(output)
-    // expect(output).is.a.string
-    // expect(output).eql(expected)
+    expect(output).is.a.string
+    expect(output).eql(expected)
   })
-
-  // it('base dpr', function () {
-  //   var fixture = '.a { height: 75px; }'
-  //   var expected = '.a {\n  height: 25rpx;\n}'
-  //   var output = postcss().use(adaptive({ baseDpr: 3 })).process(fixture).css
-  //   expect(output).is.a.string
-  //   expect(output).eql(expected)
-  // })
-
-  // it('rem precision', function () {
-  //   var fixture = '.a { height: 65rpx; }'
-  //   var expected = '.a {\n  height: 0.86666667rpx;\n}'
-  //   var output = postcss().use(adaptive({ remPrecision: 8 })).process(fixture).css
-  //   expect(output).is.a.string
-  //   expect(output).eql(expected)
-  // })
+  // px 不转换
+  it('base dpr', function () {
+    var fixture = '.a { height: 75px; }'
+    var expected = '.a {\n  height: 75px;\n}'
+    var output = postcss().use(adaptive({ baseDpr: 3 })).process(fixture).css
+    expect(output).is.a.string
+    expect(output).eql(expected)
+  })
+  // rpx 转换 精确度
+  it('rem precision', function () {
+    var fixture = '.a { height: 65rpx; }'
+    var expected = '.a {\n  height: 0.86666667rem;\n}'
+    var output = postcss().use(adaptive({ remPrecision: 8 })).process(fixture).css
+    expect(output).is.a.string
+    expect(output).eql(expected)
+  })
 })
